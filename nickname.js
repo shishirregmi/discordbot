ded264a31253aef9981d0f6364d30d44540d2892
Shishir Regmi <rshishir21@gmail.com>
2023-05-22 06:45:37 +0545

    nickname feature

---
+++ nickname.js
@@ -0,0 +1,11 @@
+const nickname = (username) => {
+  switch (username) {
+    case "broshan":
+      return "Todke";
+    case "amit":
+      return "Tyape";
+    case "roshang":
+      return "Wiz Kafle";
+  }
+};
+module.exports = nickname;
